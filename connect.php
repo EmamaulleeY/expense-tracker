ca937bee4c17fc81e8e8ffe088902632acbd0379
EmamaulleeY <96075066+EmamaulleeY@users.noreply.github.com>
2021-12-30 14:02:04 +0400

    expense

--- connect.php
+++ connect.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'userregistration.sql';
+
 #connection is established with phpmyadmin and the database
 $conn = mysqli_connect('localhost', 'root', '' );
 mysqli_select_db($conn, 'userregistration');
